abf5cbbcfff56ad9ffc4abfac6936d44d6ff5cde
Dynn <dynn27@gmail.com>
2017-05-23 14:18:14 +0800

    Add: redux

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import App from './containers/App.js';
  
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    React.createElement(Counter),
-    document.getElementById('mount')
+    React.createElement(App),
+    document.getElementById('root')
   );
 });
